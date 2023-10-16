82aee6efbc1c4f091495d20ebff9851e6ad65607
luis <luismatos138@gmail.com>
2023-10-16 22:48:46 +0100

    custom modules, event driven architecture

--- nodejsbasics/app.js
+++ nodejsbasics/app.js
@@ -1,18 +1,24 @@
+//CORE MODULES
+
 const readline = require("readline")
 const fs = require("fs")
 const http = require("http")
 const { error } = require("console")
+const url = require("url")
+const replaceHtml = require("./Modules/replaceHtml")
+
+//USER DEFINED MODULES
 
 
-// CLASS 4 - READ INPUTS FROM TERMINAL
+// // CLASS 4 - READ INPUTS FROM TERMINAL
 // const rl = readline.createInterface({
 //     input: process.stdin,
 //     output: process.stdout
 // })
 
 // rl.question("Please enter your name:", (name) => {
-//     name === "leonor" ? console.log("é uma pascácia") : 
-//     console.log("you entered: " + name)
+//     name === "leonor" ? console.log("A Leonor é a venerada da Pipoca e da Luz porque lhe deu de comer! A Leonor já devia estar a fazer os deveres de casa!!!") : 
+//     console.log("O teu nome é : " + name)
 //     rl.close()
 // })
 
@@ -20,6 +26,7 @@ const { error } = require("console")
 //     console.log("Interface closed")
 //     process.exit(0)
 // })
+
 
 
 // CLASS 5 Sync
@@ -48,60 +55,136 @@ const { error } = require("console")
 // })
 // console.log("reading file")
 
-const html = fs.readFileSync("./Template/index.html","utf-8")
+const html = fs.readFileSync("./Template/index.html", "utf-8")
 
 
 let products = JSON.parse(fs.readFileSync("./Data/Products.json", "utf-8"))
+let productsListHtml = fs.readFileSync("./Template/product-list.html", "utf-8")
+let productsDetailHtml = fs.readFileSync("./Template/product-details.html", "utf-8")
 
 
 
-const server = http.createServer((request,response) =>{
+// function replaceHtml(template, product) {
 
-let path = request.url
-if(path === "/" || path.toLocaleLowerCase() === `/home`){
-    response.writeHead(200, {
-        "Content-Type" : "text/html",
-        "my-header" : "have a nice day you headers watcher!!!"
-    })
-    response.end(html.replace(`{{%CONTENT%}}`,`You are at homepage`))
-} else if (path.toLocaleLowerCase() === "/about") {
-    response.writeHead(200, {
-        "Content-Type" : "text/html",
-        "my-header" : "have a nice day you headers watcher!!!"
-    })
-    response.end(html.replace(`{{%CONTENT%}}`,`You are at About Page`))
-} else if(path.toLocaleLowerCase() === `/contact`) {
-    response.writeHead(200, {
-        "Content-Type" : "text/html",
-        "my-header" : "have a nice day you headers watcher!!!"
-    })
-    response.end(html.replace(`{{%CONTENT%}}`,`You are at contacts page`))
-} else if (path.toLocaleLowerCase() === `/products` ){
-fs.readFile("./Data/Products.json", "utf-8", (error, data) => {
-    response.writeHead(200, {
-        "Content-Type" : "application/json",
-        "my-header" : "The data!"
-    })    
-    } )
-    response.end("You are at Products page")
-    console.log(products)
-}
+//         let output = template.replace("{{%IMAGE%}}", product.productImage)
+//         output = output.replace("{{%NAME%}}", product.name)
+//         output = output.replace("{{%MODELNAME%}}", product.modelName)
+//         output = output.replace("{{%MODELNO%}}", product.modelNumber)
+//         output = output.replace("{{%SIZE%}}", product.size)
+//         output = output.replace("{{%CAMERA%}}", product.camera)
+//         output = output.replace("{{%PRICE%}}", product.price)
+//         output = output.replace("{{%COLOR%}}", product.color)
+//         output = output.replace("{{%ID%}}", product.id)
+//         output = output.replace("{{%ROM%}}", product.ROM)
+//         output = output.replace("{{%DESC%}}", product.Description)
+    
+//         return output
+    
+// }
+
+// const server = http.createServer((request, response) => {
+//     let {query, pathname: path} =url.parse(request.url,true)
+// // console.log(x)
+//     // let path = request.url
+//     if (path === "/" || path.toLocaleLowerCase() === `/home`) {
+//         response.writeHead(200, {
+//             "Content-Type": "text/html",
+//             "my-header": "have a nice day you headers watcher!!!"
+//         })
+//         response.end(html.replace(`{{%CONTENT%}}`, `You are at homepage`))
+//     } else if (path.toLocaleLowerCase() === "/about") {
+//         response.writeHead(200, {
+//             "Content-Type": "text/html",
+//             "my-header": "have a nice day you headers watcher!!!"
+//         })
+//         response.end(html.replace(`{{%CONTENT%}}`, `You are at About Page`))
+//     } else if (path.toLocaleLowerCase() === `/contact`) {
+//         response.writeHead(200, {
+//             "Content-Type": "text/html",
+//             "my-header": "have a nice day you headers watcher!!!"
+//         })
+//         response.end(html.replace(`{{%CONTENT%}}`, `You are at contacts page`))
+//     } else if (path.toLocaleLowerCase() === `/products`) {
+//         if (!query.id){
+//            let productHtmlArray = products.map((prod) => {
+//             return replaceHtml(productsListHtml, prod)
+//             })
+//         let productResponseHtml = html.replace("{{%CONTENT%}}", productHtmlArray.join(","))
+//         response.writeHead(200, {"Content-Type": "text/html" })
+//         response.end(productResponseHtml)
+//         } else {
+//             let prod = products[query.id]
+//             let productDetailResponseHtml = replaceHtml(productsDetailHtml, prod)
+//             response.end(html.replace("{{%CONTENT%}}", productDetailResponseHtml))
+//         } 
+
+//     }
 
 
- else {
-    response.writeHead(404, {
-        "Content-Type" : "text/html",
-        "my-header" : "aqui não há nada meu irmão"
-    })
-    response.end(html.replace(`{{%CONTENT%}}`,`Error 404: page does not exist`))
-}
+//     else {
+//         response.writeHead(404, {
+//             "Content-Type": "text/html",
+//             "my-header": "aqui não há nada meu irmão"
+//         })
+//         response.end(html.replace(`{{%CONTENT%}}`, `Error 404: page does not exist`))
+//     }
 
 
 
+// })
+const server = http.createServer()
+
+server.on("request",(request,response)=>{
+    let {query, pathname: path} =url.parse(request.url,true)
+    // console.log(x)
+        // let path = request.url
+        if (path === "/" || path.toLocaleLowerCase() === `/home`) {
+            response.writeHead(200, {
+                "Content-Type": "text/html",
+                "my-header": "have a nice day you headers watcher!!!"
+            })
+            response.end(html.replace(`{{%CONTENT%}}`, `You are at homepage`))
+        } else if (path.toLocaleLowerCase() === "/about") {
+            response.writeHead(200, {
+                "Content-Type": "text/html",
+                "my-header": "have a nice day you headers watcher!!!"
+            })
+            response.end(html.replace(`{{%CONTENT%}}`, `You are at About Page`))
+        } else if (path.toLocaleLowerCase() === `/contact`) {
+            response.writeHead(200, {
+                "Content-Type": "text/html",
+                "my-header": "have a nice day you headers watcher!!!"
+            })
+            response.end(html.replace(`{{%CONTENT%}}`, `You are at contacts page`))
+        } else if (path.toLocaleLowerCase() === `/products`) {
+            if (!query.id){
+               let productHtmlArray = products.map((prod) => {
+                return replaceHtml(productsListHtml, prod)
+                })
+            let productResponseHtml = html.replace("{{%CONTENT%}}", productHtmlArray.join(","))
+            response.writeHead(200, {"Content-Type": "text/html" })
+            response.end(productResponseHtml)
+            } else {
+                let prod = products[query.id]
+                let productDetailResponseHtml = replaceHtml(productsDetailHtml, prod)
+                response.end(html.replace("{{%CONTENT%}}", productDetailResponseHtml))
+            } 
+    
+        }
+    
+    
+        else {
+            response.writeHead(404, {
+                "Content-Type": "text/html",
+                "my-header": "aqui não há nada meu irmão"
+            })
+            response.end(html.replace(`{{%CONTENT%}}`, `Error 404: page does not exist`))
+        }
+    
+    
 })
 
 
-
-server.listen(8000, "localhost", ()=>{
+server.listen(8000, "localhost", () => {
     console.log("server is up and running")
 })
